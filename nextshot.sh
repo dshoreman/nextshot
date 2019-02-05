f534e95dead2d917c3c1c120903618b1c5d42e6c
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-02-05 16:59:34 +0000

    Share file via Nextcloud OCS API

--- nextshot.sh
+++ nextshot.sh
@@ -12,3 +12,7 @@ NC_FILENAME=$(zenity --entry --title "NextShot" --text="Enter Filename" --ok-lab
 
 echo "Uploading screenshot to $NC_URL/$NC_DIR/$NC_FILENAME..."
 curl -u $NC_USERNAME:$NC_PASSWORD $NC_URL/remote.php/dav/files/$NC_USERNAME/$NC_DIR/$NC_FILENAME --upload-file $TMP_NAME
+
+curl -u $NC_USERNAME:$NC_PASSWORD -X POST -H "OCS-APIRequest: true" \
+    -F "path=/$NC_DIR/$NC_FILENAME" -F "shareType=3" \
+    $NC_URL/ocs/v2.php/apps/files_sharing/api/v1/shares?format=json
