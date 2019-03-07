5b8dc42644ef9b849d2e589aefd6679bc3d4092d
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-03-07 22:02:35 +0000

    Add tooltip and menu placeholders

--- tray.sh
+++ tray.sh
@@ -7,4 +7,11 @@ exec 3<> $PIPE
 
 yad --notification --listen <&3 &
 
+echo "menu:\
+Capture area        |\
+Capture window      |\
+Capture full screen |\
+Quit" >&3
+
 echo "icon:camera-photo-symbolic" >&3
+echo "tooltip:Nextshot" >&3
