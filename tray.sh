b19add8b0abccd72ea1b079110be8b1c7af18f2d
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-03-08 01:28:47 +0000

    Disable middle-click to quit
    Now unnecessary as we have the Quit item in the menu for that.

--- tray.sh
+++ tray.sh
@@ -5,13 +5,13 @@ rm -f $PIPE
 mkfifo $PIPE
 exec 3<> $PIPE
 
-yad --notification --listen <&3 &
+yad --notification --listen --no-middle <&3 &
 
 echo "menu:\
-Capture area        !nextshot --selection   |\
-Capture window      !nextshot --window      |\
-Capture full screen !nextshot --fullscreen  ||\
-Quit Nextshot       !quit" >&3
+Capture area        ! nextshot --selection   |\
+Capture window      ! nextshot --window      |\
+Capture full screen ! nextshot --fullscreen  ||\
+Quit Nextshot       ! quit" >&3
 
 echo "icon:camera-photo-symbolic" >&3
 echo "tooltip:Nextshot" >&3
