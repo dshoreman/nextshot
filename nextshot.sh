98786698fdd0e50b185353830ef8879a09b8fe19
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-12-30 20:07:43 +0000

    Abort if there aren't any files to clean
    Makes sure there are files to prune before trying to count them and get
    the size, otherwise du/wc can error and/or do some weird stuff.
    
    Also tweaks the colours of the output slightly to make more sense.

--- nextshot.sh
+++ nextshot.sh
@@ -291,10 +291,16 @@ prune_cache() {
     echo "Checking for cached screenshots more than 30 days old..."
     files="$(find "$_CACHE_DIR" -maxdepth 1 -iname '20*-*-* *.*.*.png' -mtime +30)"
 
+    if [[ "${files}" == "" ]]; then
+        echo "[32;1mLooks like the cache is clean![0m"
+        echo "Nothing to do, exiting."
+        exit
+    fi
+
     count=$(echo "${files}" | wc -l)
     size=$(echo "${files}" | xargs -d '\n' du -ch | grep total$ | cut -f1)
 
-    echo "[31;1mFound ${count} cached images to delete, totalling ${size}![0m"
+    echo "[33;1mFound ${count} cached images to delete, totalling ${size}![0m"
 
     read -rp "Continue? [yN] " response
     echo
@@ -305,8 +311,8 @@ prune_cache() {
 
     echo -e "PRUNING! Please wait...\n"
     echo "${files}" | xargs -d '\n' rm -v \
-        && echo "Cache pruning complete!" \
-        || echo "Could not remove some files. Try again?"
+        && echo "[32;1mCache pruning complete![0m" \
+        || echo "[31;1mCould not remove some files. Try again?[0m"
 }
 
 delay_capture() {
